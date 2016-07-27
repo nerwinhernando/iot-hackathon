63c8f52d9f4aa4adddc3876933d805d3158030c1
Arvin Cudanin <arvin.cudanin@gmail.com>
2016-07-27 18:59:04 +0000

    refactored idiot.py pir.py

--- raspberry_app/pir.py
+++ raspberry_app/pir.py
@@ -2,33 +2,93 @@ import RPi.GPIO as GPIO
 import time
 import urllib, urllib2
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup (11, GPIO.IN) #pir
-GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP) #button
-GPIO.setup(13, GPIO.OUT) #led
-GPIO.setup(15, GPIO.OUT) #led
+try:
+	import configparser
+except ImportError:
+	from ConfigParser import ConfigParser
 
-occupied ='{"id": 3,"status": 1}'
-empty ='{"id": 3,"status": 0}'
+class Settings(object):
+	server = ""
+	occupied = ""
+	empty = ""
+	pir_pin = None
+	led1_output_pin = None
+	led2_output_pin = None
+	postEnabled = None
 
-def sendPost(status):
-	opener = urllib2.build_opener(urllib2.HTTPHandler)
-	request = urllib2.Request('http://10.136.142.173:3000/api/v1/meeting_rooms', status)
-	request.add_header('Content-Type','application/json')
-	request.get_method = lambda:'PUT'
-	url = opener.open (request)
-while True:
-	i = GPIO.input(11)
-	if i == 0:
-		print "Meeting Room is empty",i
-		GPIO.output(13,0)
-		GPIO.output(15, 0)
-		sendPost(empty)
-		time.sleep(5)
-	elif i==1:
-		print "Meeting Room is occupied",i
-		GPIO.output(13,1)
-		GPIO.output(15,1)
-		sendPost(occupied)
-		time.sleep(5)
+	def loadIniFile(self):
+		#import settings from ini file
+		config = configparser.ConfigParser()
+		config.read('idiot.ini')
+
+		server_IP = config['DEFAULT']['default_server_ip']
+		server_port = config['DEFAULT']['default_server_port']
+		server_resource = config['DEFAULT']['default_server_resource']
+
+		self.server = 'http://' + server_IP + ':' + server_port + server_resource
+
+		#load json values
+		id_value = config['PIR']['id_value']
+		occupied_status_value = config['PIR']['occupied_status_value']
+		empty_status_value = config['PIR']['empty_status_value']
+
+		self.occupied = "{\"id\""+':'+str(id_value)+","+"\"status\""+": "+str(occupied_status_value)+"}"
+		self.empty = "{\"id\""+':'+str(id_value)+","+"\"status\""+": "+str(empty_status_value)+"}"
+
+		self.pir_pin = int(config['PIR']['pir_input_pin'])
+		self.led1_output_pin = int(config['PIR']['led1_output_pin'])
+		self.led2_output_pin = int(config['PIR']['led2_output_pin'])
+
+		self.postEnabled = str(config['PIR']['http_request_enabled'])
+
+	def set_GPIO(self):
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(self.pir_pin, GPIO.IN)  # pir
+#		GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button
+		GPIO.setup(self.led1_output_pin, GPIO.OUT)  # led
+		GPIO.setup(self.led2_output_pin, GPIO.OUT)  # led
+
+
+def sendPost(server,status,postEnabled):
+	if postEnabled == "True":
+		opener = urllib2.build_opener(urllib2.HTTPHandler)
+		request = urllib2.Request(server, status)
+		request.add_header('Content-Type','application/json')
+		request.get_method = lambda:'PUT'
+		#url = opener.open (request)
+		print("PUT",server,status,"sent")
+
+def function():
+	HIGH = 1
+	LOW = 0
+
+	OCCUPIED = 1
+	EMPTY = 0
+
+	settings = Settings()
+	settings.loadIniFile()
+	settings.set_GPIO()
+
+	FLAG = None
+
+	while True:
+		pir = GPIO.input(11)
+
+		if pir == EMPTY:
+			print ("Meeting Room is empty")
+			GPIO.output(settings.led1_output_pin,LOW)
+			GPIO.output(settings.led2_output_pin, LOW)
+			if FLAG is not EMPTY:
+				sendPost(settings.server, settings.empty, settings.postEnabled)
+				FLAG = EMPTY
+			time.sleep(0.5)
+
+		elif pir == OCCUPIED:
+			print ("Meeting Room is occupied")
+			GPIO.output(settings.led1_output_pin,HIGH)
+			GPIO.output(settings.led2_output_pin,HIGH)
+			if FLAG is not OCCUPIED:
+				sendPost(settings.server, settings.empty,settings.postEnabled)
+				FLAG = OCCUPIED
+			time.sleep(0.5)
