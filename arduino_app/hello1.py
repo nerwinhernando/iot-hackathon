b8546db00666c1ecad66dad041d23393c12f982e
Arvin Cudanin <arvin.cudanin@nokia.com>
2016-07-29 13:42:35 +0800

    Removed files and relocate in raspberry_app folder

--- arduino_app/hello1.py
+++
@@ -1,8 +0,0 @@
-import time
-def function():
-    try:
-        while True:
-            print ("Hello World!")
-            time.sleep(1)
-    except:
-        print ("\nHello Module stopped!")
--- arduino_app/hello2.py
+++
@@ -1,8 +0,0 @@
-import time
-def function():
-    try:
-        while True:
-            print ("Hello There!")
-            time.sleep(1.5)
-    except:
-        print ("\nHello Module stopped!")
