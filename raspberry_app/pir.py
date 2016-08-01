79747a3a306680a1e9fe3de16560c7950ec4329b
Arvin Cudanin <arvin.cudanin@nokia.com>
2016-08-01 20:59:51 +0800

    added pir module

--- raspberry_app/pir.py
+++ raspberry_app/pir.py
@@ -4,15 +4,19 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup (11, GPIO.IN)
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 while True:
-	i=GPIO.input(11)
-	if i ==0:
-		print "No Intruders",i
-		GPIO.output(3,0)
+	i = GPIO.input(11)
+	if i == 0:
+		print "Meeting Room is empty",i
+		GPIO.output(13,0)
+		GPIO.output(15, 0)
 		time.sleep(0.1)
 	elif i==1:
-		print "Intruder Detected",i
-		GPIO.output(3,1)
+		print "Meeting Room is occupied",i
+		GPIO.output(13,0)
+		GPIO.output(15,0)
 		time.sleep(0.1)
