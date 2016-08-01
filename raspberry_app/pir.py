1071800391a3204f26a26e016de8b1bb70b9cfe2
Arvin Cudanin <arvin.cudanin@nokia.com>
2016-08-01 21:43:15 +0800

    modified pir.py to send POST

--- raspberry_app/pir.py
+++ raspberry_app/pir.py
@@ -1,12 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+import urllib, urllib2
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup (11, GPIO.IN)
-GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+GPIO.setup (11, GPIO.IN) #pir
+GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP) #button
+GPIO.setup(13, GPIO.OUT) #led
+GPIO.setup(15, GPIO.OUT) #led
+
+occupied = {
+    "id": 3,
+    "status": 1
+}
+
+empty = {
+    "id": 3,
+    "status": 0
+}
+
+url = 'http://10.136.142.173:3000/api/v1/meeting_rooms'
+
+def sendPost(status):
+	data = urllib.urlencode(status)
+	request = urllib2.Request(url, data)
+	response = urllib2.urlopen(request).read()
+	print (response)
 
 while True:
 	i = GPIO.input(11)
@@ -14,9 +33,11 @@ while True:
 		print "Meeting Room is empty",i
 		GPIO.output(13,0)
 		GPIO.output(15, 0)
-		time.sleep(0.1)
+		sendPost(empty)
+		time.sleep(5)
 	elif i==1:
 		print "Meeting Room is occupied",i
-		GPIO.output(13,0)
-		GPIO.output(15,0)
-		time.sleep(0.1)
+		GPIO.output(13,1)
+		GPIO.output(15,1)
+		sendPost(occupied)
+		time.sleep(5)
