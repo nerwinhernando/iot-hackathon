f551c94b549cedd266d42d6a02b3d6c9289cb4d3
rolcris13 <rolcris13@gmail.com>
2016-08-03 23:02:59 +0800

    get api status

--- web_app/idiotsolutions_api/app/controllers/api_meeting_rooms_controller.rb
+++ web_app/idiotsolutions_api/app/controllers/api_meeting_rooms_controller.rb
@@ -1,20 +1,34 @@
 class ApiMeetingRoomsController < ApplicationController
-  before_filter :find_reservation, only: [:show, :update]
+  before_filter :set_date_time, only: [:show, :update]
   def show
-    render json: @reserve
+    @meeting_rooms = MeetingRoom.all.as_json
+    @meeting_rooms.each_with_index do |m,i|
+      find_reservation(m["id"])
+      @reserve.each do |r|
+        @meeting_rooms[i]["status"] = r.status
+      end
+    end
+    render json: @meeting_rooms
   end
   def update
+    find_reservation(params[:id])
     @reserve.update(status: params[:status])
     render json: @reserve
   end
-  def find_reservation
+
+  private 
+
+  def set_date_time
     @date = Date.today
     @time = Time.now.to_formatted_s(:time)
-    @reservations = Reservation.where(meeting_room_id: params[:id]).where("time_start < ?", @time).where("time_end > ?", @time)
+  end
+
+  def find_reservation(id)
+    @reservations = Reservation.where(meeting_room_id: id).where("time_start < ?", @time).where("time_end > ?", @time)
     if @reservations.exists?(reservation_date: @date)
       @reserve = @reservations.where(reservation_date: @date)
     else
-      @reserve = Reservation.where(meeting_room_id: params[:id], reservation_date: nil)
+      @reserve = Reservation.where(meeting_room_id: id, reservation_date: nil)
       @reserve.update(time_start: @time, time_end: @time)
     end
     render nothing: true, status: :not_found unless @reserve.present?
